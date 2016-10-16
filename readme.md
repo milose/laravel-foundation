e288db8086f19e03d044c4c0f6caa68a41328de0
Milos Sakovic <m@deva.co>
2016-10-16 04:55:02 +0200

    Change the directory.

--- readme.md
+++ readme.md
@@ -1,6 +1,18 @@
 # Laravel Foundation package
 Use this package to override bootstrap templates for auth and home page with Zurb Foundation.
 
+# Install
+
+## Add to `config/app.php`
+```php
+Milose\LaravelFoundation\LaravelFoundationServiceProvider::class,
+```
+
+## publish views
+```bash
+php artisan vendor:publish
+```
+
 # DO NOT USE THIS YET
 
 #TODO
--- src/LaravelFoundationServiceProvider.php
+++ src/LaravelFoundationServiceProvider.php
@@ -10,8 +10,12 @@ class LaravelFoundationServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-             __DIR__.'/../resources/views' => base_path('resources/views/vendor/laravelFoundation'),
-         ], 'views');
+             __DIR__.'/../resources/views/auth' => base_path('resources/views/vendor/laravelFoundation'),
+         ], 'foundation-auth');
+
+        $this->publishes([
+             __DIR__.'/../resources/views/pagination' => base_path('resources/views/vendor/pagination'),
+         ], 'foundation-paginate');
 
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravelFoundation');
     }
