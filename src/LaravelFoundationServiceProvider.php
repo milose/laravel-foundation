4548695d5bae582875510183601cf121956ad814
Milos Sakovic <m@deva.co>
2016-10-16 05:11:40 +0200

    Change base_path to resource_path on service provider.

--- src/LaravelFoundationServiceProvider.php
+++ src/LaravelFoundationServiceProvider.php
@@ -10,11 +10,11 @@ class LaravelFoundationServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-             __DIR__.'/../resources/views/auth' => base_path('resources/views/vendor/laravelFoundation/auth'),
+             __DIR__.'/../resources/views/auth' => resource_path('views/vendor/laravelFoundation/auth'),
          ], 'foundation-auth');
 
         $this->publishes([
-             __DIR__.'/../resources/views/pagination' => base_path('resources/views/vendor/pagination'),
+             __DIR__.'/../resources/views/pagination' => resource_path('views/vendor/pagination'),
          ], 'foundation-paginate');
 
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'foundation');
